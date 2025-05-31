00c8c0802828c58f870e2fead55ee9c313e25789
Julio Henriquez <julio.m.henriquez@gmail.com>
2025-05-30 20:48:00 -0400

    Add linting support to project scripts and configurations
    Added `eslint` scripts to package.json files for API and core packages. Updated eslint configurations to include TypeScript-specific rules, ignore patterns, and parser options. Standardized linting setup for improved code quality and consistency.

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -1,11 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import tsEslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/*.js"]
+  },
+  {
+    files: ["**/*.ts"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsEslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  tsEslint.configs.recommended,
 ]);
